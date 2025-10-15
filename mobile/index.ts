c21637d52640c8b3f0cb3374e011d29be1521b97
wittachai-as <wittachai.as@paneet.co.th>
2025-10-15 11:15:48 -0400

    chore: add mobile app with Expo config and project-level .gitignore

---
+++ mobile/index.ts
@@ -0,0 +1,10 @@
+// This section was removed since these imports and registration are duplicated below
+
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
